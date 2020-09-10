c23368a3a08fdcd8b16d23606b408c4a13a9edc6
Rannyeri Baptist <rannyerirbatista@gmail.com>
2020-09-10 05:47:22 -0300

    Created services page

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -1,3 +1,31 @@
 $(".toggle-navbar").click(function() {
   $(".navbar__content").toggle();
 });
+
+window.setTimeout(function() {
+  setImageActive();
+}, 1000);
+
+$('.next-slide').click(function() {
+  $('#slider').slick('slickNext');
+  $('#images').slick('slickNext');
+  $('#small').slick('slickNext');
+
+  setImageActive();
+})
+
+$('.prev-slide').click(function() {
+  $('#slider').slick('slickPrev');
+  $('#images').slick('slickPrev');
+  $('#small').slick('slickPrev');
+
+  setImageActive();
+})
+
+function setImageActive() {
+  $('#images').find('.slick-slide').addClass('image-filter');
+  $('#images').find('.slick-active').removeClass('image-filter');
+
+  $('#small').find('.slick-slide').addClass('image-filter');
+  $('#small').find('.slick-active').removeClass('image-filter');
+}
